e7f5aeedee2d19eb81eeaaa373c416ed938ec284
OmarRyad <omarproductions46@gmail.com>
2020-09-14 14:23:08 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import CounterExample from './Components/CounterExample';
+//import Counter from './Components/CounterExample';
+import HelloWorld from './Components/HelloWorld'
+import Header from './Components/Header'
+
+function App() {
+  return (
+    <div >
+      <Header />
+      <HelloWorld type="plane"/>
+
+     { /* <CounterExample /> */} 
+
+    </div>
+  );
+}
+
+export default App;
