99bc2594bf169a5efd55bbf2dd09c14220429b6e
OmarRyad <omarproductions46@gmail.com>
2020-09-14 20:12:10 +0200

    s

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import HelloWorld from './Components/HelloWorld'
 import Header from './Components/Header'
 
+
 function App() {
   return (
     <div >
--- src/Components/HelloWorld.js
+++ src/Components/HelloWorld.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class HelloWorld extends Component {
     render() {
         return (
-        <h1>Hello {this.props.type}</h1>
+        <h1 className="p-3 font-bold border-b">Hello {this.props.type}</h1>
         )
     }
 }
